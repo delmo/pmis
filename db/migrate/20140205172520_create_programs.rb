8052e4f6da6da46b0266d95c99622c01c66126b1
Ryan Delmo <rtgd20@sussex.ac.uk>
2014-02-07 17:58:22 +0000

    Rollback db:migrate to VERSION=0

--- db/migrate/20140205172520_create_programs.rb
+++ db/migrate/20140205172520_create_programs.rb
@@ -1,5 +1,5 @@
 class CreatePrograms < ActiveRecord::Migration
-  def change
+  def up
     create_table :programs do |t|
       t.integer "user_id"
       t.integer "department_id"
@@ -30,4 +30,7 @@ class CreatePrograms < ActiveRecord::Migration
     add_index("programs", "source_id")
   end
 
+  def down
+   drop_table :programs
+  end
 end
